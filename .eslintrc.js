56be0fe01b7609b220d43762ff3c48ebf3ef6c5e
zzuhann <zzuhanlin@gmail.com>
2024-04-20 15:41:03 +0800

    add prettier and adjust eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
+		'prettier',
+		'plugin:prettier/recommended',
+		'plugin:react/recommended',
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['react-refresh'],
+	rules: {
+		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+		'prettier/prettier': 'off',
+		'react-hooks/rules-of-hooks': 'error',
+		'react-hooks/exhaustive-deps': 'warn',
+		'react/react-in-jsx-scope': 'off',
+		'no-console': 'error',
+		'@typescript-eslint/no-unused-vars': 'warn',
+	},
+};
