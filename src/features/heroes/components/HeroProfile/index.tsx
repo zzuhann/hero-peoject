f178da6f18faa64f649eb62d262c16e2bcbb8446
zzuhann <zzuhanlin@gmail.com>
2024-04-24 00:56:18 +0800

    fix: wrap heroProvider on correct position

--- src/features/heroes/components/HeroProfile/index.tsx
+++ src/features/heroes/components/HeroProfile/index.tsx
@@ -2,36 +2,34 @@ import { ContainerStyles, RightContainerStyles, SaveButtonStyles } from './style
 import { Typography, CircularProgress } from '@mui/material';
 import AbilityCounterList from './components/AbilityCounterList';
 import { useContext } from 'react';
-import { HeroesContext, HeroesProvider } from '@/context/heroesContext';
+import { HeroesContext } from '@/context/heroesContext';
 
 const HeroProfile = () => {
 	const { isHeroProfileLoading, remaining, saveProfile, isUpdateHeroProfileLoading } =
 		useContext(HeroesContext);
 
 	return (
-		<HeroesProvider>
-			<ContainerStyles>
-				{isHeroProfileLoading && <CircularProgress />}
-				{!isHeroProfileLoading && (
-					<>
-						<AbilityCounterList />
-						<RightContainerStyles>
-							<Typography textAlign='right' variant='subtitle1' color='#3d3d3e'>
-								剩餘點數：{remaining}
-							</Typography>
-							<SaveButtonStyles
-								variant='contained'
-								disabled={remaining > 0}
-								onClick={saveProfile}
-								loading={isUpdateHeroProfileLoading}
-							>
-								儲存
-							</SaveButtonStyles>
-						</RightContainerStyles>
-					</>
-				)}
-			</ContainerStyles>
-		</HeroesProvider>
+		<ContainerStyles>
+			{isHeroProfileLoading && <CircularProgress />}
+			{!isHeroProfileLoading && (
+				<>
+					<AbilityCounterList />
+					<RightContainerStyles>
+						<Typography textAlign='right' variant='subtitle1' color='#3d3d3e'>
+							剩餘點數：{remaining}
+						</Typography>
+						<SaveButtonStyles
+							variant='contained'
+							disabled={remaining > 0}
+							onClick={saveProfile}
+							loading={isUpdateHeroProfileLoading}
+						>
+							儲存
+						</SaveButtonStyles>
+					</RightContainerStyles>
+				</>
+			)}
+		</ContainerStyles>
 	);
 };
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,13 +2,21 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import HeroesLayout from '@/layout';
 import HeroProfile from '@/features/heroes/components/HeroProfile';
 import NotFoundPage from '@/pages/notFound';
+import { HeroesProvider } from '@/context/heroesContext';
 
 const AppRoutes = () => {
 	return (
 		<Routes>
 			<Route path='/' element={<Navigate to='/heroes' />} />
 			<Route path='/heroes' element={<HeroesLayout />}>
-				<Route path=':id' element={<HeroProfile />} />
+				<Route
+					path=':id'
+					element={
+						<HeroesProvider>
+							<HeroProfile />
+						</HeroesProvider>
+					}
+				/>
 			</Route>
 			<Route path='*' element={<NotFoundPage />} />
 		</Routes>
