b413cc379e2d619c246d472e4afe470096b236f5
zzuhann <zzuhanlin@gmail.com>
2024-04-24 00:18:24 +0800

    test: remove App test

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import { expect, test } from 'vitest';
-
-test('go to / route, it should navigate to /heroes route', () => {
-	render(<App />);
-
-	expect(screen.getByText(/123/i)).toBeInTheDocument();
-});
